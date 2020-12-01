b5b7053c45199d8ecb57918d608d544821a2edb0
Igor Kling <kling.igor@gmail.com>
2020-12-01 19:18:54 +0400

    simple form example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Form, Field } from 'react-final-form'
+
+class App extends React.Component {
+
+  onSubmit = (values: any) => {
+    console.log(values)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form
+          onSubmit={this.onSubmit}
+          // validate={validate}
+          render={({ handleSubmit, submitting, pristine, form }) => (
+            <form onSubmit={handleSubmit}>
+              <Field
+                name="name"
+                render={({ input, meta }) => (
+                  <div>
+                    <label>Name</label>
+                    <input {...input} />
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </div>
+                )}
+              />
+              <Field
+                name="comment"
+                render={({ input, meta }) => (
+                  <div>
+                    <label>Comment</label>
+                    <textarea {...input} />
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </div>
+                )}
+              />
+              <button type="submit" disabled={submitting || pristine}>
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
+            </form>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
