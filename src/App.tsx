097255808c778942bf718aa45d649f9492c65dd0
Igor Kling <kling.igor@gmail.com>
2020-12-01 21:18:22 +0400

    strict form types

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 
 import { Form, Field } from 'react-final-form'
+import { Config } from "final-form";
+
+enum FieldNames {
+  NAME = 'name',
+  COMMENT = 'comment'
+}
+
+interface FormValue {
+  [FieldNames.NAME]?: string;
+  [FieldNames.COMMENT]?: string;
+}
+
+const initialValues: Config<FormValue>["initialValues"] = {
+  [FieldNames.NAME]: 'initial name',
+  [FieldNames.COMMENT]: 'initial comment'
+}
 
 class App extends React.Component {
 
-  onSubmit = async (values: any): Promise<any> => {
+  onSubmit: Config<FormValue>["onSubmit"] = async (values: FormValue): Promise<any> => {
     console.log(values)
-
     return new Promise(resolve => setTimeout(resolve, 1000))
   }
 
@@ -16,11 +31,11 @@ class App extends React.Component {
       <div className="App">
         <Form
           onSubmit={this.onSubmit}
-          initialValues={{ name: 'initial name', comment: 'initial comment' }}
+          initialValues={initialValues}
           render={({ handleSubmit, submitting, pristine, form }) => (
             <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
               <Field
-                name="name"
+                name={FieldNames.NAME}
                 render={({ input, meta }) => (
                   <div>
                     <label>Name</label>
@@ -30,7 +45,7 @@ class App extends React.Component {
                 )}
               />
               <Field
-                name="comment"
+                name={FieldNames.COMMENT}
                 render={({ input, meta }) => (
                   <div>
                     <label>Comment</label>
@@ -45,7 +60,7 @@ class App extends React.Component {
                 </button>
                 <button
                   type="button"
-                  onClick={form.reset}
+                  onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => form.reset()}
                   disabled={submitting || pristine}
                   style={{ width: '8rem' }}
                 >
